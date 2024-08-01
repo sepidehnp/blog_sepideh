d6937b656288cdfcd3366907356bb55605bf1ebf
sepideh <sepidehnp.67@gmail.com>
2024-08-01 15:59:27 +0330

    session 2

--- Modules/Spd/Home/Resources/Views/layouts/master.blade.php
+++ Modules/Spd/Home/Resources/Views/layouts/master.blade.php
@@ -2,14 +2,17 @@
 <html class="no-js" lang="en">
     <head>
         @include('Home::section.meta')
-
         <title>@yield('title') | {{ config('app.name') }}</title>
 
         @include('Home::section.css')
     </head>
 <body>
+    <body>
         <!-- Preloader Start -->
         @include('Home::section.preloader')
+        <div class="main-wrap">
+            <!--Offcanvas sidebar-->
+            @include('Home::section.sidebar')
 
 <div class="main-wrap">
     <!--Offcanvas sidebar-->
@@ -1547,5 +1550,16 @@
 <!-- Vendor JS-->
     @include('Home::section.js')
 </body>
+  <!-- Main Header -->
+  @include('Home::section.header')
 
+  <!-- Main Wrap Start -->
+  @yield('content')
+
+  <!-- Footer Start-->
+  @include('Home::section.footer')
+</div>
+<div class="dark-mark"></div>
+@include('Home::section.js')
+</body>
 </html>
